ebfbc047d0e80a3abbdacf8b47b3685a415992b1
DESKTOP-HA97GMI\SSAFY <foottable@gmail.com>
2025-07-29 13:13:25 +0900

    chore: 백엔드 프로젝트 생성 및 의존성 설치 (S13P11A202-136)

---
+++ server/src/test/java/kr/kro/areuhot/AreuhotApplicationTests.java
@@ -0,0 +1,13 @@
+//package kr.kro.areuhot;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AreuhotApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
