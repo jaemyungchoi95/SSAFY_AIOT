e3d919837b618e2cc8d7fddd91e932dbcb556c9c
kalcal1509 <kalcal1509@naver.com>
2025-07-30 17:44:50 +0900

    feat: rack data and List fetching and rendering added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,9 @@
+import { useState } from 'react';
 import Map from '../components/common/Map';
 import Sidebar from '../components/common/Sidebar';
 import './Home.css';
-import React from 'react';
-import { useState } from 'react';
 
 const Home = () => {
-  // const [message, setMessage] = useState("서버에서 응답을 기다리는 중...");
-
-  // useEffect(() => {
-  //   axios.get(`${import.meta.env.VITE_REACT_APP_SPRING_BASE_URL}/get`)
-  //     .then(response => {
-  //       setMessage(response.data);
-  //     })
-  //     .catch(error => {
-  //       console.error('axios 데이터 로딩 에러', error);
-  //       setMessage("데이터 불러오기에 실패하였습니다.");
-  //     });
-  // }, []); // 의존성 배열 꼭 넣기!
-
   const [selectedWarehouse, setSelectedWarehouse] = useState(1);
   const [dangerCnt, setDangerCnt] = useState(0);
   const [cautionCnt, setCautionCnt] = useState(0);
--- frontend/src/stores/useUserStore.jsx
+++ frontend/src/stores/useUserStore.jsx
@@ -1,28 +1,35 @@
-import { create } from "zustand"
+import { create } from 'zustand';
 
-export const useUserStore = create(set => ({
-// 1. 관리할 상태 (state)
-isLoggedIn: false,
-user: null,
+export const useUserStore = create((set) => ({
+  // 1. 관리할 상태 (state)
+  username: null,
+  name: null,
+  role: null,
+  isLoggedIn: false,
+  error: null,
 
-// 2. 상태를 변경하는 함수 (actions)
-// 액션 함수들은 `set` 함수를 사용하여 상태를 업데이트합니다.
-// `set` 함수는 기존 상태를 인자로 받아 새로운 상태를 반환하는 함수를 전달받습니다.
-
-/**
- * 사용자를 로그인 처리하는 액션
- * @param {object} userData - 로그인할 사용자의 정보 (예: { name: '홍길동', email: 'hong@gildong.com' })
- */
-    login: (userData) => set((state) => ({
+  // 2. 상태를 변경하는 함수 (actions)
+  login: async ({ username, name, role }) => {
+    try {
+      set({
+        username,
+        name,
+        role,
         isLoggedIn: true,
-        user: userData,
-    })),
+        error: null,
+      });
+    } catch (error) {
+      set({ error: '로그인에 실패하였습니다.' });
+    }
+  },
 
-/**
- * 사용자를 로그아웃 처리하는 액션
- */
-    logout: () => set((state) => ({
-        isLoggedIn: false,
-        user: null,
-    })),
+  logout: () => {
+    set({
+      username: null,
+      name: null,
+      role: null,
+      isLoggedIn: false,
+      error: null,
+    });
+  },
 }));
