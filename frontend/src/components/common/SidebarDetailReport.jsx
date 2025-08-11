fb4733f5554fa74ad22bd98d1b60e68f784cc2eb
DESKTOP-DLOPF19\SSAFY <wpolice3120@naver.com>
2025-08-11 13:43:26 +0900

    Feat:로그인 API 및 로그인 관련 기능 수정

--- frontend/src/components/common/SidebarDetailReport.jsx
+++ frontend/src/components/common/SidebarDetailReport.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './SidebarDetailReport.css';
 import ReportContent from './ReportContent';
 import ReportBtn from './ReportBtn';
+import { useUserStore } from '../../stores/useUserStore';
 
 const SidebarDetailReport = ({ alert, onEditClick }) => {
+  const currentUserId = useUserStore((state) => state.userId);
+  const isMyPost = currentUserId === alert.writerId;
   return (
     <div className="SidebarDetailReport">
       <div className="SidebarDetailReport_Header">
         <div className="SidebarDetailReport_Title">처리 내역</div>
-        <ReportBtn text={'수정하기'} onClick={onEditClick} />
+        {isMyPost && <ReportBtn text={'수정하기'} onClick={onEditClick} />}
       </div>
       <ReportContent alert={alert} />
     </div>
