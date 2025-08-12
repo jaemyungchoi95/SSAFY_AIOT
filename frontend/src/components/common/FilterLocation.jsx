4a2d24b62d7d4c30006153a0173b63afe087d70b
DESKTOP-DLOPF19\SSAFY <wpolice3120@naver.com>
2025-08-12 17:51:11 +0900

    Fix:모바일 환경 front 수정

--- frontend/src/components/common/FilterLocation.jsx
+++ frontend/src/components/common/FilterLocation.jsx
@@ -25,7 +25,9 @@ const FilterLocation = () => {
         className="FilterLocation_Title"
         onClick={() => setIsOpen((prev) => !prev)}
       >
-        <span>{defaultName}</span>
+        <span className="inline-block max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis">
+          {defaultName}
+        </span>
         <span className="Chevron">▼</span>
       </button>
       <div className="LocationUnderline"></div>
