37a9ad5aabd10e996362cb3b5c1eaa88e14f4317
jaemyungchoi95 <woaud130@gmail.com>
2025-08-15 17:52:08 +0900

    fix: 모달 입력창 수정 중

--- frontend/src/components/common/IssueModal.jsx
+++ frontend/src/components/common/IssueModal.jsx
@@ -6,16 +6,17 @@ import Modal_Content_Right from './ModalContentRight';
 
 const IssueModal = ({ onClose }) => {
   return (
-    <div className="IssueModal_Backdrop" onClick={onClose}>
+    <div className="IssueModal_Backdrop flex items-center justify-center z-[1000] fixed top-0 left-0 w-[100%] h-[100%] bg-[rgba(0, 0, 0, 0.4)]"
+         onClick={onClose}>
       <div className="IssueModal" onClick={(e) => e.stopPropagation()}>
-        <div className="Modal_Header">
+        <div className="Modal_Header flex-shrink-0 mb-3">
           <ModalHeader />
         </div>
-        <div className="Modal_Content">
-          <div className="Modal_Content_Left_Box">
+        <div className="Modal_Content flex flex-1 overflow-hidden mb-3 gap-4">
+          <div className="Modal_Content_Left_Box border-r-2 border-[#424242] flex-[3] min-w-0">
             <Modal_Content_Left />
           </div>
-          <div className="Modal_Content_Right_Box">
+          <div className="Modal_Content_Right_Box flex-[1] min-w-0">
             <Modal_Content_Right />
           </div>
         </div>
