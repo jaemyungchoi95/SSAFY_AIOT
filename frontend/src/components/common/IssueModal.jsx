4c383f8a392f1ee5c983161f4650f2f80ebfbb54
jaemyungchoi95 <woaud130@gmail.com>
2025-08-16 00:54:44 +0900

    fix: 작성하기, 수정하기 뷰 수정

--- frontend/src/components/common/IssueModal.jsx
+++ frontend/src/components/common/IssueModal.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import './IssueModal.css';
 import ModalHeader from './ModalHeader';
 import Modal_Content_Left from './ModalContentLeft';
 import Modal_Content_Right from './ModalContentRight';
 
 const IssueModal = ({ onClose }) => {
   return (
-    <div className="IssueModal_Backdrop flex items-center justify-center z-[1000] fixed top-0 left-0 w-[100%] h-[100%] bg-[rgba(0, 0, 0, 0.4)]"
+    <div className="IssueModal_Backdrop flex items-center justify-center z-[1000] fixed top-0 left-0 w-[100%] h-[100%] bg-[rgba(0,0,0,0.4)]"
          onClick={onClose}>
-      <div className="IssueModal" onClick={(e) => e.stopPropagation()}>
+      <div className="IssueModal flex flex-col bg-[#20212a] rounded-2xl w-[90vw] h-[90vh] max-w-[1800px] border-2 border-[#424242]" onClick={(e) => e.stopPropagation()}>
         <div className="Modal_Header flex-shrink-0 mb-3">
           <ModalHeader />
         </div>
         <div className="Modal_Content flex flex-1 overflow-hidden mb-3 gap-4">
-          <div className="Modal_Content_Left_Box border-r-2 border-[#424242] flex-[3] min-w-0">
+          <div className="Modal_Content_Left_Box border-r-2 border-[#424242] flex-3 min-w-0">
             <Modal_Content_Left />
           </div>
-          <div className="Modal_Content_Right_Box flex-[1] min-w-0">
+          <div className="Modal_Content_Right_Box flex-1 min-w-0">
             <Modal_Content_Right />
           </div>
         </div>
